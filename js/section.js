834399ffe13eb4f45e5670a26a525fdd70fde7a8
Nguyen Duc Thien <nguyenducthienfu@gmail.com>
2023-03-15 17:39:04 +0700

    productions update, thiếu thanh lăn ở dưới, sẽ bổ sung sau

---
+++ js/section.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 10,
+    autoplay: {
+        delay:7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides:true,
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
