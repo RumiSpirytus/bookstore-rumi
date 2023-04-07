da7489a0a2979af216881a9238351f50a22210ca
Nguyen Duc Thien <nguyenducthienfu@gmail.com>
2023-04-08 01:55:19 +0700

    create action, reducers and constant with redux, fix bug load

--- fe/src/components/homeComponents/ShopSection.js
+++ fe/src/components/homeComponents/ShopSection.js
@@ -1,19 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Pagination from "./pagination";
-import axios from "axios"
+import {useDispatch, useSelector} from "react-redux"
+import { listProduct } from "../../Redux/Actions/ProductActions";
+import Loading from './../LoadingError/Loading';
+import Message from "../LoadingError/Error.js";
 //fetch products
 const ShopSection = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+
+const productList = useSelector((state) => state.productList)
+const {loading, error, products } = productList
 
   useEffect(() => {
-    const fetchproducts = async () => {
-      const {data} = await axios.get("/api/products")
-      setProducts(data)
-    }
-    fetchproducts()
-  }, []);
+    dispatch(listProduct());
+  }, [dispatch]);
   return (
     <>
       <div className="container">
@@ -21,7 +23,15 @@ const ShopSection = () => {
           <div className="row">
             <div className="col-lg-12 col-md-12 article">
               <div className="shopcontainer row">
-                {products.map((product) => (
+                {loading ? (
+                  <div className="mb-5">
+                    <Loading />
+                  </div>
+                  ) : error ? (
+                  <Message variant="alert-danger">{error}</Message>
+                    ): (
+                    <>
+                    {products.map((product) => (
                   <div
                     className="shop col-lg-4 col-md-6 col-sm-6"
                     key={product._id}
@@ -49,6 +59,10 @@ const ShopSection = () => {
                     </div>
                   </div>
                 ))}
+                    </>
+                  )
+                }
+                
                 {/* Pagination */}
                 <Pagination />
               </div>
