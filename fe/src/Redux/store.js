8776cb5abed64c1eb3e91a74b962090dff602cd4
Nguyen Duc Thien <nguyenducthienfu@gmail.com>
2023-04-08 00:42:19 +0700

    redux connected

---
+++ fe/src/Redux/store.js
@@ -0,0 +1,18 @@
+import {  combineReducers, applyMiddleware } from "redux"
+import { legacy_createStore as createStore } from 'redux';
+import thunk from "redux-thunk"
+import { composeWithDevTools } from "redux-devtools-extension"
+
+const reducer = combineReducers({})
+
+const initialState = {}
+
+const middleWare = [thunk]
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleWare))
+)
+
+export default store
