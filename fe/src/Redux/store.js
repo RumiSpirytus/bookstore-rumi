cd5bd2986b8a06bd50a6adf20c0eb1a6de80d6f9
Nguyen Duc Thien <nguyenducthienfu@gmail.com>
2023-04-11 02:04:18 +0700

    add redux to cart, not add to cart in quantity

--- fe/src/Redux/store.js
+++ fe/src/Redux/store.js
@@ -3,13 +3,24 @@ import { legacy_createStore as createStore } from 'redux';
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
 import { productDetailsReducer, productListReducer } from "./Reducers/ProductReducers";
+import { cartReducer } from "./Reducers/CartReducers";
+import { CART_ADD_ITEM } from './Constants/CartConstants';
 
 const reducer = combineReducers({
     productList : productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer,
 })
 
-const initialState = {}
+const cartItemsFromLocalStorage = localStorage.getItem('cartItems')
+? JSON.parse(localStorage.getItem('cartItems'))
+:[]
+
+const initialState = {
+    cart: {
+        cartItems: cartItemsFromLocalStorage,
+    }
+}
 
 const middleWare = [thunk]
 
