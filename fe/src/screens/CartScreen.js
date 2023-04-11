36a07a153406e30c8ee890209840c2c6381dea26
Nguyen Duc Thien <nguyenducthienfu@gmail.com>
2023-04-11 14:48:24 +0700

    add to cart success

--- fe/src/screens/CartScreen.js
+++ fe/src/screens/CartScreen.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { addToCart } from "../Redux/Actions/cartActions";
 import { useDispatch, useSelector } from 'react-redux';
 
-const CartScreen = ({match, location}) => {
+const CartScreen = ({match, location, history}) => {
   window.scrollTo(0, 0);
   const dispatch = useDispatch();
   const productId = match.params.id
@@ -13,11 +13,20 @@ const CartScreen = ({match, location}) => {
   const cart = useSelector((state) => state.cart)
   const {cartItems } = cart
   
+  const total = cartItems.reduce((a, i) => a + i.qty * i.price,0).toFixed(2);
+
   useEffect(() => {
     if(productId){
       dispatch(addToCart(productId, qty))
     }
   }, [dispatch, productId, qty])
+
+  const checkOutHandler = () => {
+      history.push("/login?redirect=shipping")
+  }
+  const removeFromCartHandle = () => {
+    //
+}
 
   return (
     <> 
@@ -45,53 +54,66 @@ const CartScreen = ({match, location}) => {
               <div className=" alert alert-info text-center mt-3">
           Total Cart Products
           <Link className="text-success mx-2" to="/cart">
-            (4)
+            ({cartItems.length})
           </Link>
         </div>
         {/* cartiterm */}
-        <div className="cart-iterm row">
-          <div className="remove-button d-flex justify-content-center align-items-center">
+        {
+          cartItems.map((item) => (
+            <div className="cart-iterm row">
+          <div 
+          onClick={removeFromCartHandle}
+          className="remove-button d-flex justify-content-center align-items-center">
             <i className="fas fa-times"></i>
           </div>
           <div className="cart-image col-md-3">
-            <img src="/img/bia-sach1.jpg" alt="nike" />
+            <img src={item.image} alt={item.name} />
           </div>
           <div className="cart-text col-md-5 d-flex align-items-center">
-            <Link to="#">
-              <h4>Ánh trăng đêm hè</h4>
+            <Link to={`/product/${item.product}`}>
+              <h4>{item.name}</h4>
             </Link>
           </div>
           <div className="cart-qty col-md-2 col-sm-5 mt-md-5 mt-3 mt-md-0 d-flex flex-column justify-content-center">
             <h6>QUANTITY</h6>
-            <select>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-            </select>
+            <select value={item.qty} 
+                      onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </select>
           </div>
           <div className="cart-price mt-3 mt-md-0 col-md-2 align-items-sm-end align-items-start  d-flex flex-column justify-content-center col-sm-7">
-            <h6>SUBTOTAL</h6>
-            <h4>$456</h4>
+            <h6>PRICE</h6>
+            <h4>${item.price}</h4>
           </div>
         </div>
+          ))
+        }
+        
 
         {/* End of cart iterms */}
         <div className="total">
           <span className="sub">total:</span>
-          <span className="total-price">$567</span>
+          <span className="total-price">${total}</span>
         </div>
         <hr />
         <div className="cart-buttons d-flex align-items-center row">
           <Link to="/" className="col-md-6 ">
             <button>Continue To Shopping</button>
           </Link>
-          <div className="col-md-6 d-flex justify-content-md-end mt-3 mt-md-0">
-            <button>
-              <Link to="/shipping" className="text-white">
+          {
+            total > 0 && (
+              <div className="col-md-6 d-flex justify-content-md-end mt-3 mt-md-0">
+            <button onClick={checkOutHandler}>
                 Checkout
-              </Link>
             </button>
           </div>
+            )
+          }
+          
         </div>
       
             </>
