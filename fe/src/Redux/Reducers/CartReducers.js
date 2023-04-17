9031ff28471be64d84b195b63893752374ab7eb4
Nguyen Duc Thien <nguyenducthienfu@gmail.com>
2023-04-18 02:16:03 +0700

    payment screen

--- fe/src/Redux/Reducers/CartReducers.js
+++ fe/src/Redux/Reducers/CartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../Constants/CartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../Constants/CartConstants";
 
 export const cartReducer = 
     (state = {cartItems:[], shippingAddress: {}}, 
@@ -30,6 +30,11 @@ export const cartReducer =
             return {
                 ...state,
                 shippingAddress: action.payload
+            }
+        case CART_SAVE_PAYMENT_METHOD:
+            return {
+                ...state,
+                paymentMethod: action.payload
             }
         default:
             return state; 
